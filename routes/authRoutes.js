c34b27cdfbda4e596e05ae73824517f7e31d9bce
heardMan <markyheard@gmail.com>
2019-03-09 01:49:47 -0800

    add

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -95,7 +95,7 @@ router.post("/register", function (req, res) {
         models.User.create(userInstance)
             .then(function (resp) {
                 //send success message
-                res.json({ message: "Creation Sucess!", id: resp.id }).redirect("/login");
+                res.json({ message: "Creation Sucess!", id: resp.id });
             })
             .catch(function (err) {
                 //send error message
