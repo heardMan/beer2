0b97e1160502ee2645e976c37c5c8dc56d1f0977
heardMan <markyheard@gmail.com>
2019-03-08 23:58:20 -0800

    added more files

--- models/user.model.js
+++ models/user.model.js
@@ -1,56 +1,61 @@
 module.exports = function (sequelize, DataTypes) {
-  var User = sequelize.define("User", {
-      email: {
-          type: DataTypes.STRING,
-          unique: true,
-          required: true,
-          validate: {
-              isEmail: true
-          }
-      },
-      firstName: {
-          type: DataTypes.STRING,
-          required: true
-      },
-      lastName: {
-          type: DataTypes.STRING,
-          required: true
-      },
-      salt: DataTypes.STRING,
-      hash: DataTypes.STRING,
-      address1: {
-          type: DataTypes.STRING,
-          required: true
-      },
-      address2: {
-          type: DataTypes.STRING,
-          required: true
-      },
-      city: {
-          type: DataTypes.STRING,
-          required: true
-      },
-      state: {
-          type: DataTypes.STRING,
-          required: true
-      },
-      zip: {
-          type: DataTypes.STRING,
-          required: true
-      },
-      senator1: {
+    var User = sequelize.define("User", {
+        email: {
+            type: DataTypes.STRING,
+            unique: true,
+            required: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            required: true
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            required: true
+        },
+        salt: DataTypes.STRING,
+        hash: DataTypes.STRING,
+        address1: {
+            type: DataTypes.STRING,
+            required: true
+        },
+        address2: {
+            type: DataTypes.STRING,
+            required: true
+        },
+        city: {
+            type: DataTypes.STRING,
+            required: true
+        },
+        state: {
+            type: DataTypes.STRING,
+            required: true
+        },
+        zip: {
+            type: DataTypes.STRING,
+            required: true
+        },
+        senator1: {
+            type: DataTypes.STRING,
+            required: false
+        },
+        senator2: {
+            type: DataTypes.STRING,
+            required: false
+        },
+        usRepresentative: {
+            type: DataTypes.STRING,
+            required: false
+        },
+        congressionalDistrict: {
           type: DataTypes.STRING,
           required: false
       },
-      senator2: {
-          type: DataTypes.STRING,
-          required: false
-      },
-      usRepresentative: {
-          type: DataTypes.STRING,
-          required: false
-      },
-      phone: DataTypes.STRING
-  });
-  return User;
-};
+        phone: DataTypes.STRING
+    });
+    return User;
+  };
+  
