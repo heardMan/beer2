6eca9935a8568b0c8398a4db0697daa0cf617416
heardMan <markyheard@gmail.com>
2019-03-09 01:08:12 -0800

    add

--- models/user.model.js
+++ models/user.model.js
@@ -24,7 +24,7 @@ module.exports = function (sequelize, DataTypes) {
         },
         address2: {
             type: DataTypes.STRING,
-            required: true
+            
         },
         city: {
             type: DataTypes.STRING,
