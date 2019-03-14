4d70be096810063de15940fa65818347bbde4861
heardMan <markyheard@gmail.com>
2019-03-13 22:06:33 -0700

    updated styling

--- routes/helpers/verifyToken.js
+++ routes/helpers/verifyToken.js
@@ -5,7 +5,7 @@ var models = require("../../models");
 //
 function verifyToken(req, res, next) {
   //get cookie with useerId
-  
+
   //get cookie with user token
   var token = req.cookies.token;
   //check if token exists
@@ -15,22 +15,22 @@ function verifyToken(req, res, next) {
   }
   //find user from id stored in the cookie
 
-      jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
-        if (err) {
-          return res.status(500).send({
-            auth: false,
-            message: "Failed to authenticate token."
-          });
-        }
-        // if everything good, save to request for use in other routes
-        req.userId = decoded.userID;
-
-        next();
+  jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(500).send({
+        auth: false,
+        message: "Failed to authenticate token."
       });
+    }
+    // if everything good, save to request for use in other routes
+    req.userId = decoded.userID;
+
+    next();
+  });
 
 
 
-    
+
 }
 
 module.exports = verifyToken;
